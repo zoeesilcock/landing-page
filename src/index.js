61ef3be5b142c1c8669a400a7d71312904a1c835
Zoee Silcock <mrzoee@gmail.com>
2016-01-26 11:51:24 +0100

    Use some bootstrap markup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-var foo = require('./styles/main.scss');
-
 if (module.hot) {
-  var webpackHotMiddlewareClient = require("webpack-hot-middleware/client");
+  var webpackHotMiddlewareClient = require('webpack-hot-middleware/client');
   webpackHotMiddlewareClient.subscribe(function (message) {
     if (message.reload === true) {
       window.location.reload();
